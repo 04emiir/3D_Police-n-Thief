7f00123c403a9e6b3964c599d87ae6f043ae5773
Emilio José Marín Romero <emilio.nero3@gmail.com>
2023-05-22 12:43:43 +0200

    juego

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,55 +1,97 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
-    bool isSelectedThief = false;
-    bool isSelectedPolice = false;
+    bool isTurnThief = true;
+    bool isTurnPolice = false;
+
+    public TextMeshProUGUI currentTurn;
 
     private GameObject selectedPolice;
+    private GameObject thiefGO;
     void Start() {
 
     }
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetMouseButtonDown(0)) {
-            RaycastHit raycastHit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out raycastHit, 100f)) {
-                if (raycastHit.transform != null) {
-                    ClickedPiece(raycastHit.transform.gameObject);
-                }
-            }
-        }
+        //if (Input.GetMouseButtonDown(0)) {
+        //    RaycastHit raycastHit;
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    if (Physics.Raycast(ray, out raycastHit, 100f)) {
+        //        if (raycastHit.transform != null) {
+        //            CheckPossibleMovement(raycastHit.transform.gameObject);
+        //        }
+        //    }
+        //}
     }
 
-    public void ClickedPiece(GameObject gameObject) {
-        if (gameObject.tag == "Thief") {
-            isSelectedThief = !isSelectedThief;
-            isSelectedPolice = false;
-
+    public void CheckCurrentTurn() {
+        if (isTurnThief) {
+            currentTurn.text = "Turno Ladrón";
+            currentTurn.color = Color.red;
+        } else {
+            currentTurn.text = "Turno Policías";
+            currentTurn.color = Color.blue;
         }
 
-        if (gameObject.tag == "Police") {
-            isSelectedThief = false;
-            isSelectedPolice = !isSelectedThief;
-            selectedPolice = gameObject;
-            if (isSelectedPolice) {
-                selectedPolice.GetComponent<Outline>().enabled = true;
-            } else {
-                selectedPolice.GetComponent<Outline>().enabled = false;
-            }
+    }
 
+    //public void CheckPossibleMovement(GameObject gameObject) {
+    //    if (gameObject.tag == "Thief") {
+    //        isSelectedThief = !isSelectedThief;
+    //        isSelectedPolice = false;
+
+    //    }
+
+    //    if (gameObject.tag == "Police") {
+    //        isSelectedThief = false;
+    //        isSelectedPolice = !isSelectedThief;
+    //        selectedPolice = gameObject;
+    //        if (isSelectedPolice) {
+    //            selectedPolice.GetComponent<Outline>().enabled = true;
+    //        } else {
+    //            selectedPolice.GetComponent<Outline>().enabled = false;
+    //        }
+
+    //    }
+
+    //    if (gameObject.tag == "Tile" && (isSelectedPolice || isSelectedThief)) {
+    //        if(isSelectedPolice)
+    //            MovePiece(selectedPolice, gameObject.transform);
+    //        if(isSelectedThief)
+    //            MovePiece(GameObject.FindGameObjectWithTag("Thief"), gameObject.transform);
+    //    }
+    //}
+
+    public void CheckMovementThief() {
+        // Betweewn (1,0,1) and (8,0,8)
+        /* (x+1, y, z+1) right+up
+         * (x+1, y, z-1) right+down
+         * (x-1, y, z+1) left+up
+         * (x-1, y, z-1) left+down
+         */
+        float thiefPositionRow = thiefGO.transform.position.z;
+        float thiefPositionColumn = thiefGO.transform.position.x;
+
+        if ((thiefPositionColumn + 1) <= 8 && (thiefPositionColumn + 1) <= 8) {
+            //can move (x+1, y, z+1) right+up
+        }
+        if ((thiefPositionColumn + 1) <= 8 && (thiefPositionColumn - 1) >= 1) {
+            //can move  (x+1, y, z-1) right+down
+        }
+        if ((thiefPositionColumn - 1) >= 1 && (thiefPositionColumn - 1) >= 1) {
+            //can move (x-1, y, z+1) left+up
+        }
+        if ((thiefPositionColumn - 1) >= 8 && (thiefPositionColumn + 1) <= 8) {
+            //can move (x-1, y, z-1) left+down
         }
 
-        if (gameObject.tag == "Tile" && (isSelectedPolice || isSelectedThief)) {
-            if(isSelectedPolice)
-                MovePiece(selectedPolice, gameObject.transform);
-            if(isSelectedThief)
-                MovePiece(GameObject.FindGameObjectWithTag("Thief"), gameObject.transform);
-        }
+
+
     }
 
     public void MovePiece (GameObject piece, Transform newPosition) {
--- Assets/Scripts/GenerateBoard.cs
+++ Assets/Scripts/GenerateBoard.cs
@@ -11,14 +11,14 @@ public class GenerateBoard : MonoBehaviour {
     private GameObject tile;
     private Dictionary<int, string> tiles = new Dictionary<int, string>();
     void Start() {
-        tiles.Add(1, "a");
-        tiles.Add(2, "b");
-        tiles.Add(3, "c");
-        tiles.Add(4, "d");
-        tiles.Add(5, "e");
-        tiles.Add(6, "f");
-        tiles.Add(7, "g");
-        tiles.Add(8, "h");
+        tiles.Add(1, "A");
+        tiles.Add(2, "B");
+        tiles.Add(3, "C");
+        tiles.Add(4, "D");
+        tiles.Add(5, "E");
+        tiles.Add(6, "F");
+        tiles.Add(7, "G");
+        tiles.Add(8, "H");
 
         PopulateBoard();
         PlacePolices();
@@ -26,34 +26,34 @@ public class GenerateBoard : MonoBehaviour {
     }  
 
     private void PopulateBoard() {
-        for (var row = 1; row < 9; row++) {
-            for (var column = 1; column < 9; column++) {
-                if (column % 2 == 0) {
-                    if (row % 2 == 0)
-                        tile = Instantiate(blackFloor, new Vector3(row, 0f, column), Quaternion.identity);
+        for (var rowNumber = 8; rowNumber > 0; rowNumber--) {
+            for (var columnLetter = 8; columnLetter > 0; columnLetter--) {
+                if (columnLetter % 2 == 0) {
+                    if (rowNumber % 2 == 0)
+                        tile = Instantiate(blackFloor, new Vector3(columnLetter, 0f, rowNumber), Quaternion.identity);
                     else
-                        tile = Instantiate(whiteFloor, new Vector3(row, 0f, column), Quaternion.identity);
+                        tile = Instantiate(whiteFloor, new Vector3(columnLetter, 0f, rowNumber), Quaternion.identity);
                 } else {
-                    if (row % 2 == 0)
-                        tile = Instantiate(whiteFloor, new Vector3(row, 0f, column), Quaternion.identity);
+                    if (rowNumber % 2 == 0)
+                        tile = Instantiate(whiteFloor, new Vector3(columnLetter, 0f, rowNumber), Quaternion.identity);
                     else
-                        tile = Instantiate(blackFloor, new Vector3(row, 0f, column), Quaternion.identity);
+                        tile = Instantiate(blackFloor, new Vector3(columnLetter, 0f, rowNumber), Quaternion.identity);
                 }
                 tile.transform.parent = GameObject.Find("Board").transform;
-                tile.name = "Tile" + row + "-" + tiles[column];
+                tile.name = "Tile" + tiles[columnLetter] + "-" + rowNumber;
             }
         }
     }
 
     private void PlaceThief() {
-        Transform thiefSpawn = GameObject.Find("Tile1-d").transform;
+        Transform thiefSpawn = GameObject.Find("TileD-1").transform;
         Instantiate(thief, new Vector3(thiefSpawn.transform.position.x, thiefSpawn.transform.position.y+thief.transform.localScale.y/2 + 0.5f, thiefSpawn.transform.position.z), Quaternion.identity);
     }
     private void PlacePolices() {
-        Transform policeSpawnA = GameObject.Find("Tile8-a").transform;
-        Transform policeSpawnB = GameObject.Find("Tile8-c").transform;
-        Transform policeSpawnC = GameObject.Find("Tile8-e").transform;
-        Transform policeSpawnD = GameObject.Find("Tile8-g").transform;
+        Transform policeSpawnA = GameObject.Find("TileA-8").transform;
+        Transform policeSpawnB = GameObject.Find("TileC-8").transform;
+        Transform policeSpawnC = GameObject.Find("TileE-8").transform;
+        Transform policeSpawnD = GameObject.Find("TileG-8").transform;
         Instantiate(police, new Vector3(policeSpawnA.transform.position.x, policeSpawnA.transform.position.y + police.transform.localScale.y / 2 + 0.5f, policeSpawnA.transform.position.z), Quaternion.identity);
         Instantiate(police, new Vector3(policeSpawnB.transform.position.x, policeSpawnB.transform.position.y + police.transform.localScale.y / 2 + 0.5f, policeSpawnB.transform.position.z), Quaternion.identity);
         Instantiate(police, new Vector3(policeSpawnC.transform.position.x, policeSpawnC.transform.position.y + police.transform.localScale.y / 2 + 0.5f, policeSpawnC.transform.position.z), Quaternion.identity);
